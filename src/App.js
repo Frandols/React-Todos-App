ca70830e8624dd4f99c48e7a1085ee7f9e80488d
Francisco <frandelosantoss12@gmail.com>
2021-02-20 22:14:18 -0300

    Adjusting to original paths on App.js

--- src/App.js
+++ src/App.js
@@ -97,7 +97,7 @@ const App = () => {
   return (
     <Router>
 
-      <Route exact path='/React-Todos-App/'>
+      <Route exact path='/'>
         <div className='app'>
           <Nav userName={userName} userPhoto={userPhoto}/>
             <div className='title'>
@@ -108,9 +108,9 @@ const App = () => {
         </div>
       </Route>
 
-      <Route path='/React-Todos-App/register'>
+      <Route path='/register'>
 
-      { name_storaged ? <Redirect to='/React-Todos-App/'/> : <Redirect to='/React-Todos-App/register'/> }
+      { name_storaged ? <Redirect to='/'/> : <Redirect to='/register'/> }
 
         <Register setUserName={setUserName} setUserPhoto={setUserPhoto} userName={userName} userPhoto={userPhoto}/>
       </Route>
